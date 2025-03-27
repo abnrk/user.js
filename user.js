bce1739c05f4f1170da6220e25ed60eae89ec696
abnrk <adudowas@gmail.com>
2025-03-27 19:01:07 +0000

    Create user.js

---
+++ user.js
@@ -0,0 +1,39 @@
+// Telemetry
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry",false);
+user_pref("browser.newtabpage.activity-stream.telemetry",false);
+user_pref("browser.ping-centre.telemetry",false);
+user_pref("datareporting.healthreport.service.enabled",false);
+user_pref("datareporting.healthreport.uploadEnabled",false);
+user_pref("datareporting.policy.dataSubmissionEnabled",false);
+user_pref("datareporting.sessions.current.clean",true);
+user_pref("devtools.onboarding.telemetry.logged",false);
+user_pref("toolkit.telemetry.archive.enabled",false);
+user_pref("toolkit.telemetry.bhrPing.enabled",false);
+user_pref("toolkit.telemetry.enabled",false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled",false);
+user_pref("toolkit.telemetry.hybridContent.enabled",false);
+user_pref("toolkit.telemetry.newProfilePing.enabled",false);
+user_pref("toolkit.telemetry.prompted",2);
+user_pref("toolkit.telemetry.rejected",true);
+user_pref("toolkit.telemetry.reportingpolicy.firstRun",false);
+user_pref("toolkit.telemetry.server","");
+user_pref("toolkit.telemetry.shutdownPingSender.enabled",false);
+user_pref("toolkit.telemetry.unified",false);
+user_pref("toolkit.telemetry.unifiedIsOptIn",false);
+user_pref("toolkit.telemetry.updatePing.enabled",false);
+user_pref("toolkit.telemetry.coverage.opt-out",true);
+user_pref("toolkit.coverage.opt-out",true);
+user_pref("browser.search.serpEventTelemetry.enabled",false);
+user_pref("browser.search.serpEventTelemetryCategorization.enabled",false);
+user_pref("network.captive-portal-service.enabled",false);
+user_pref("captivedetect.canonicalURL","");
+user_pref("network.connectivity-service.enabled",false);
+user_pref("network.connectivity-service.IPv4.url","");
+user_pref("network.connectivity-service.IPv6.url","");
+user_pref("browser.safebrowsing.downloads.remote.enabled",false);
+
+// Enable adding custom search engines
+user_pref("browser.urlbar.update2.engineAliasRefresh",true);
+
+// Cache
+user_pref("media.memory_cache_max_size",65536);
